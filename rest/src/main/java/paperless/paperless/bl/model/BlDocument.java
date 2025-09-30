56c37b996d320c17023fe60d07e612ea49a0c5f3
LucaBonavetti <161732253+LucaBonavetti@users.noreply.github.com>
2025-09-30 17:09:33 +0200

    Revert "added validator"
    This reverts commit b0067dbf3ae6d0bf6fafd4bc674c2d6c2567a95c.

--- rest/src/main/java/paperless/paperless/bl/model/BlDocument.java
+++ rest/src/main/java/paperless/paperless/bl/model/BlDocument.java
@@ -4,30 +4,19 @@ import java.time.OffsetDateTime;
 
 public class BlDocument {
     private Long id;
-
-    @NotBlank(message = "filenmae must not be blank")
     private String filename;
-
     private String contentType;
-
-    @Positive(message = "size must be > 0")
     private long size;
-
-    @NotNull(message = "uploadedAt must not be null")
     private OffsetDateTime uploadedAt;
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getFilename() { return filename; }
     public void setFilename(String filename) { this.filename = filename; }
-
     public String getContentType() { return contentType; }
     public void setContentType(String contentType) { this.contentType = contentType; }
-
     public long getSize() { return size; }
     public void setSize(long size) { this.size = size; }
-
     public OffsetDateTime getUploadedAt() { return uploadedAt; }
     public void setUploadedAt(OffsetDateTime uploadedAt) { this.uploadedAt = uploadedAt; }
 }
